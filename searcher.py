41a8ef69c95a24089987a3b15fa180eda93509df
cyrusin <lysh0526@gmail.com>
2012-11-13 14:58:06 +0800

    Add methods to access db(SQLite)

--- searcher.py
+++ searcher.py
@@ -1,23 +1,48 @@
 import urllib2
+import sqlite3 as sqlite
 from bs4 import BeautifulSoup
 from urlparse import urljoin
 ignorewords=set(['the','of','to','and','a','in','is','it'])
 class crawler:
 	def __init__(self,dbname):
-		pass
+		self.con=sqlite.connect(dbname)
+		
 	def __del__(self):
-		pass
-
+		self.con.close()
 	def dbcommit(self):
-		pass
+		self.con.commit()
 	def getentryid(self,table,field,value,addnew=True):
 		return None
 	def createindex(self,url,soup):
+		if url.isindexed(): return 
 		print 'Indexing %s' % url
+		
+		textfromurl=self.gettext(soup)
+		words=self.sprwords(textfromurl)
+		urlid=self.getentryid('urltable','url',url)
+		for i in range(len(words)):
+			word=words[i]
+			if word in ignorewords: continue
+			wordid=self.getentryid('wordtable','word',word)
+			self.con.execute("insert into wordlocationinurl(urlid,wordid,location) values (%d,%d,%d)" % (urlid,wordid,i))
+		
 	def gettext(self,soup):
+		contentstring=soup.strings
+		if contentstring==None:
+			spcontents=soup.contents
+			result=''
+			for content in  spcontents:
+				result=result+self.gettext(content)+'\n'
+			return result
+		else:
+			return contentstring.strip()
+
+			
 		return None
+	//分词
 	def sprwords(self,text):
-		return None
+		splitter=re.compile('\\W*')
+		return [s.lower() for s in splitter.split(text) if s!='']
 
 	def isindexed(self,url):
 		return False
@@ -49,7 +74,14 @@ class crawler:
 				self.dbcommit()
 			pages=newpages	
 	def createindextables(self):
-		pass
-
-
-
+		self.con.execute('create table urltable(url)')
+		self.con.execute('create table wordtable(word)')
+		self.con.execute('create table wordlocationinurl(urlid,wordid,location)')
+		self.con.execute('create table linktable(fromid integer,toid integer)')
+		self.con.execute('create table wordforlink(wordid,linkid)')
+		self.con.execute('create index wordidx on wordtable(word)')
+		self.con.execute('create index urlidx on urltable(url)')
+		self.con.execute('create index wordurlidx on wordlocationinurl(wordid)')
+		self.con.execute('create index urltoidx on linktable(toid)')
+		self.con.execute('create index urlfromidx on linktable(fromid)')
+		self.dbcommit()
