6dd9f67e06ae6c068efb4230168b8335ee29ced1
cyrusin <lysh0526@gmail.com>
2012-12-21 14:18:17 +0800

    'pass'

--- getstopwords.py
+++
@@ -1,9 +0,0 @@
-def get():
-	stopwords = []
-	for line in file('stop_words_eng.txt'):
-		word = line.strip()
-		stopwords.append(word)
-	if len(stopwords) != 0:
-		print 'All is done!count:'
-		print len(stopwords)
-		print stopwords
