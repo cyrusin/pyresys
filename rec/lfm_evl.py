7e5f440b04a73844fe3a64f9a6657861d38914e4
cyrusin <lysh0526@gmail.com>
2013-01-03 15:50:13 +0800

    3rd training model

--- rec/lfm_evl.py
+++ rec/lfm_evl.py
@@ -8,7 +8,7 @@ import latent_factor_model
 import cPickle as pickle
 
 # Load test, result
-path = '/home/lishuai/work/lfm/test2/'
+path = '/home/lishuai/work/lfm/test4/'
 
 fte = open(path+'testset', 'r')
 test = pickle.load(fte)
