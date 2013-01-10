74efdabb05252ca64e1e5d943fe0289507c3d635
cyrusin <lysh0526@gmail.com>
2013-01-10 17:04:23 +0800

    modify crawl.py to make it more interactive

--- search/crawl.py
+++ search/crawl.py
@@ -3,11 +3,13 @@
 '''This module is used to crawing the web pages and 
 build index for all the pages.
 '''
-
 import searcher
-
 crawler = searcher.crawler('index.db')
 crawler.createindextables()
-urls = ['http://kiwitobes.com/wiki/Categorical_list_of_programming_languages.html']
+while 1:
+    seed_urls = raw_input("Crawling from these urls(divided with ';'):")
+    #urls = ['http://kiwitobes.com/wiki/Categorical_list_of_programming_languages.html']
+    if seed_urls is not None:
+        urls = seed_urls.split(';')
+        break
 crawler.crawl(urls)
-
