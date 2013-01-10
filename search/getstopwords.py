4c244706de682f994fa51b745c06c2bf8ac715cb
cyrusin <lysh0526@gmail.com>
2013-01-10 10:21:47 +0800

    add method to get stop words

--- search/getstopwords.py
+++ search/getstopwords.py
@@ -1,9 +1,26 @@
-def get():
-	stopwords = []
-	for line in file('stop_words_eng.txt'):
-		word = line.strip()
-		stopwords.append(word)
-	if len(stopwords) != 0:
-		print 'All is done!count:'
-		print len(stopwords)
-		print stopwords
+#!/usr/bin/env python
+# filename: getstopwords.py
+'''This module is used to get the stop words and pickle it.'''
+import pickle
+try:
+    datafile = open('stop_words_eng.txt')
+except IOError , e:
+    print e
+else:
+    stopwords=[]
+    
+    for line in datafile:
+    	word=line.strip()
+    	stopwords.append(word)
+    
+    if len(stopwords) != 0:
+        f = open('/home/lishuai/LSGitRepo/PCI_Project/stopwords_pickle', 'w')
+        pickle.dump(stopwords, f)
+        f.close()
+        print 'All is done!'
+    else:
+        print "There is no stop words."
+finally:
+    datafile.close()
+
+
