687e4caf5dba3aeff4aa80dc308d71a4ce08d988
cyrusin <lysh0526@gmail.com>
2013-01-10 16:58:44 +0800

    build the main module to response to  the query

---
+++ search/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# filename: main.py
+'''This is module is the main method to run the query.'''
+import searcher
+query_obj = searcher.querying('index.db')
+if query_obj is not None:
+    while 1:
+        query = raw_input('Searching for:')
+        if query is not None:
+            break
+    query_obj.queryrank(query)
+else:
+    print 'Something wrong with the database!'
